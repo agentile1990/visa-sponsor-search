3a3d0e4115e96037d0f092094fe972297c24b6a5
Andrew Gentile <agentile1990@gmail.com>
2021-01-30 10:39:25 -0600

    Initial Migration Improvements
    - Fix cityId Data Type on companies Table
    - Case Sensitive Column Names
    - Foreign keys
    - Unique Indexes

--- db_deploy/migrations/00_0001_Initial_Schema.sql
+++ db_deploy/migrations/00_0001_Initial_Schema.sql
@@ -4,51 +4,54 @@
 
 DROP TABLE IF EXISTS "countries";
 CREATE TABLE "countries" (
-    id SERIAL PRIMARY KEY,
+    "id" SERIAL PRIMARY KEY,
 
-    name TEXT UNIQUE NOT NULL,
-    code3 TEXT UNIQUE NOT NULL,
+    "name" TEXT UNIQUE NOT NULL,
+    "code3" TEXT UNIQUE NOT NULL,
 
-    createdAt TIMESTAMP DEFAULT NOW(),
-    updatedAt TIMESTAMP DEFAULT NOW()
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "updatedAt" TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS "cities";
 CREATE TABLE "cities" (
-    id UUID PRIMARY KEY,
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    name TEXT UNIQUE NOT NULL,
+    "name" TEXT UNIQUE NOT NULL,
 
-    countryId INTEGER NOT NULL,
+    "countryId" INTEGER NOT NULL REFERENCES "countries" ("id"),
 
-    createdAt TIMESTAMP DEFAULT NOW(),
-    updatedAt TIMESTAMP DEFAULT NOW()
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "updatedAt" TIMESTAMP DEFAULT NOW()
 );
+CREATE UNIQUE INDEX ON "cities" ("name", "countryId");
 
 DROP TABLE IF EXISTS "companies";
 CREATE TABLE "companies" (
-    id UUID PRIMARY KEY,
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    name TEXT NOT NULL,
+    "name" TEXT NOT NULL,
 
-    countryId INTEGER NOT NULL,
-    cityId INTEGER NOT NULL,
+    "countryId" INTEGER NOT NULL REFERENCES "countries" ("id"),
+    "cityId" UUID NOT NULL REFERENCES "cities" ("id"),
 
-    createdAt TIMESTAMP DEFAULT NOW(),
-    updatedAt TIMESTAMP DEFAULT NOW()
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "updatedAt" TIMESTAMP DEFAULT NOW()
 );
+CREATE UNIQUE INDEX ON"companies" ("name", "countryId", "cityId");
 
 DROP TABLE IF EXISTS "sponsorships";
 CREATE TABLE "sponsorships" (
-    id UUID PRIMARY KEY,
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    countryId INTEGER NOT NULL,
-    cityId UUID NOT NULL,
-    companyId UUID NOT NULL,
+    "countryId" INTEGER NOT NULL REFERENCES "countries" ("id"),
+    "cityId" UUID NOT NULL REFERENCES "cities" ("id"),
+    "companyId" UUID NOT NULL REFERENCES "companies" ("id"),
 
-    type TEXT,
-    route TEXT,
+    "type" TEXT,
+    "route" TEXT,
     
-    createdAt TIMESTAMP DEFAULT NOW(),
-    updatedAT TIMESTAMP DEFAULT NOW()
+    "createdAt" TIMESTAMP DEFAULT NOW(),
+    "updatedAT" TIMESTAMP DEFAULT NOW()
 );
+CREATE UNIQUE INDEX ON "sponsorships" ("companyId", "type");
