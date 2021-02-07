4ed164d2cc737e5ad583c79b1c692221c8444325
Andrew Gentile <agentile1990@gmail.com>
2021-02-06 22:23:51 -0600

    Add Basic Error Handling

--- server/app.js
+++ server/app.js
@@ -13,4 +13,13 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+app.use('/*', function (req, res) {
+    return res.status(404).send({ message: 'Not Found' });
+});
+
+app.use(function (error, req, res, next) {
+    console.error('Unhandled Error:', error);
+    return res.status(500).send({ message: 'Internal Server Error' });
+});
+
 module.exports = app;
