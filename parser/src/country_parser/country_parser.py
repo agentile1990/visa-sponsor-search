4ea7aa766bd2425a8d8dc71f9b492a8ed1f6dd69
Andrew Gentile <agentile1990@gmail.com>
2021-03-03 22:43:25 -0600

    Refactor Models into Base Class
    - Add non-SQL enforceable constraints to parser sub-class

--- parser/src/country_parser/country_parser.py
+++ parser/src/country_parser/country_parser.py
@@ -2,10 +2,21 @@
 Abstract base class for all country parsers
 """
 
+import models
+
 
 class CountryParser():
     def __init__(self, name):
         self.name = name
 
+        self.city_model = models.City
+
+        self.company_model = models.Company
+
+        self.sponsorship_model = models.Sponsorship
+
     def parse(self):
         print("[{}] parse not implemented".format(self.name))
+
+    def upsertSponsorship(self, data):
+        self.sponsorship_model.upsert(data)
